5467071eb0d8fbb374248725637eedb5d18b590b
sevensuii <severyn00@gmail.com>
2021-05-31 20:35:11 +0100

    Creando metodos para inicio de sesion y registro

--- Python/Trim3/FP_interfaz_hotel/controlador/Controlador.py
+++ Python/Trim3/FP_interfaz_hotel/controlador/Controlador.py
@@ -3,4 +3,11 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from modelo.Modelo import *
+import modelo.Modelo as mo
+
+def iniciaSesionC(u, c):
+    valor = mo.compruebaUsuario(u, c)
+    return valor
+
+def registraUsuarioC(u, d, e, c):
+    pass
