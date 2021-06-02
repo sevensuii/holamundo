1a8a7c1353230dbb473f5fb0956eaea09322f5c6
sevensuii <severyn00@gmail.com>
2021-06-02 22:26:58 +0100

    Correccion de simples errores

--- Python/Trim3/FP_interfaz_hotel/controlador/Controlador.py
+++ Python/Trim3/FP_interfaz_hotel/controlador/Controlador.py
@@ -10,4 +10,22 @@ def iniciaSesionC(u, c):
     return valor
 
 def registraUsuarioC(u, d, e, c):
-    pass
+    usr1 = str(u)
+    dni1 = str(d).upper()
+    email1 = str(e)
+    contra1 = str(c)
+    usr2 = str(mo.consultaUsuario(u)).lower()
+    dni2 = str(mo.consultaDNI(d)).lower()
+    msg_info = ''
+    if usr1.lower() == usr2.lower():
+        msg_info = 'El usuario ya existe, pruebe con otro.'
+        return 1, msg_info
+    elif dni1.upper() == dni2.upper():
+        msg_info = 'El dni que ha introducido ya se encuentra registrado.'
+        return 2, msg_info
+    else:
+        mo.creaUsuario(usr1, dni1, email1, contra1)
+        msg_info = 'El usuario se ha creado correctamente!'
+        return 2, msg_info
+
+registraUsuarioC('Saraa', '23456789P', 'dasdad', '123')
--- Python/Trim3/FP_interfaz_hotel/modelo/Modelo.py
+++ Python/Trim3/FP_interfaz_hotel/modelo/Modelo.py
@@ -27,9 +27,52 @@ def compruebaUsuario(usuario, contrasena):
 def cierraBaseDAtos():
     basedatos.close()
 
+def consultaUsuario(usuario):
+    try:
+        with basedatos.cursor() as cursor:
+            consulta = 'SELECT nik FROM usuarios WHERE nik LIKE %s'
+            cursor.execute(consulta, (usuario))
+            usuario2 = str(cursor.fetchall())
+            usuario2 = usuario2.strip("(),'")
+
+            return str(usuario2)
+    finally:
+        None
+
+def consultaDNI(dni1):
+    try:
+        with basedatos.cursor() as cursor:
+            consulta = 'SELECT dni FROM usuarios WHERE dni LIKE %s'
+            cursor.execute(consulta, (dni1))
+            dni2 = str(cursor.fetchall())
+            dni2 = dni2.strip("(),'")
+
+            return str(dni2)
+    finally:
+        None
+
+def creaUsuario(usr, dni, email, contra):
+    try:
+        with basedatos.cursor() as cursor:
+            consulta1 = 'SELECT id_usuario FROM usuarios ORDER BY id_usuario DESC LIMIT 1'
+            cursor.execute(consulta1)
+            ultimo_id = str(cursor.fetchall())
+            ultimo_id = int(ultimo_id.strip("(),'"))
+            ultimo_id = ultimo_id + 1
+            consulta2 = 'INSERT INTO usuarios(id_usuario, dni, nik, clave, email, rol) VALUES(%s, %s, %s, %s, %s, %s)'
+            cursor.execute(consulta2, (ultimo_id, dni, usr, contra, email, 'usr'))
+        basedatos.commit()
+
+            #return str(dni2)
+    finally:
+        None
+
+#INSERT INTO usuarios(id_usuario, dni, nik, clave, email, rol) 
+#VALUES(5, '32546532O', 'pepe', 'pepa', 'pepe@gmail.com', 'usr')
+
 #compruebaUsuario('Linda', '1111')
-
-
+#consultaDNI('45322197B')
+#creaUsuario('maria', '23234587E', 'maria@gmail.com', 'qekl')
 
 # Conexion a la base de datos
 #try:
