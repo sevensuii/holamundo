b5d1393258c0a842ce771a0de407fd41ccf179b0
sevensuii <severyn00@gmail.com>
2021-06-09 23:51:55 +0100

    Funciones para comunicar a la vista con el modelo

--- Python/Trim3/FP_interfaz_hotel/controlador/Controlador.py
+++ Python/Trim3/FP_interfaz_hotel/controlador/Controlador.py
@@ -36,3 +36,45 @@ def recibeDatosC(opcion):
     elif opcion == 2:
         resultado = mo.recibeHabitaciones()
         return resultado
+
+def aniadeHabitacionC(m2, piso, cat):
+    metros = int(m2.get())
+    pi = int(piso.get())
+    categ = str(cat.get())
+    print(metros)
+    print(pi)
+    print(categ)
+
+    mo.aniadeHabitacionM(metros, pi, categ)
+
+def eliminaDatosC(ind, id):
+    valor = id[0]
+    if ind == 0:
+        mo.eliminaCliente(valor)
+    elif ind == 1:
+        mo.eliminaHabitacion(valor)
+
+def creaClienteC(nombre, apellidos, dni, tlf, email, fecha, pais, dire):
+    nombre1 = str(nombre.get())
+    apellidos1 = str(apellidos.get())
+    dni1 = str(dni.get())
+    tlf1 = str(tlf.get())
+    email1 = str(email.get())
+    fecha1 = str(fecha.get())
+    pais1 = str(pais.get())
+    dire1 = str(dire.get())
+    mo.creaClienteM(nombre1, apellidos1, dni1, tlf1, email1, fecha1, pais1, dire1)
+
+def devuelveDatos(ind, id):
+    if ind == 0:
+        datos = mo.devuelveDatosClienteM(int(id[0]))
+    elif ind == 1:
+        datos = mo.devuelveDatosHab(int(id[0]))
+
+    return datos
+
+def editaDatosHabC(m2, cat, piso, ide):
+        m21 = m2.get()
+        cat1 = cat.get()
+        piso1 = piso.get()
+        mo.actualizaHab(int(ide),m21, cat1, piso1)
