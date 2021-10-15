d014bea1e3092aadb7fcf66ec7f25b90292a1c8f
sevensuii <severyn00@gmail.com>
2021-10-15 09:55:11 +0100

    Created a working timer but needs some changes

--- 2year/DEW/domAndEvents/scripts/script.js
+++ 2year/DEW/domAndEvents/scripts/script.js
@@ -1,1 +1,29 @@
-//console.log('hola mundo');
+var ms = 0;
+var s = 0;
+var m = 0;
+//Change this piece for a time object
+
+
+function timer(){
+    var msAux, sAux, mAux;
+    ms += 2;
+    if (ms>100){
+        s++;
+        ms=0;
+    }
+    if (s>59){
+        m++;
+        s=0;
+    }
+
+    if (s<10){sAux="0"+s;}else{sAux=s;}
+    if (m<10){mAux="0"+m;}else{mAux=m;}
+    if (ms<10){msAux="0"+ms;}else{msAux=ms;}
+
+    document.getElementById("timer").innerHTML = mAux + ":" + sAux + ":" + msAux; 
+}
+
+function timerStart(){
+    timer();
+    setInterval(timer,20);
+}
