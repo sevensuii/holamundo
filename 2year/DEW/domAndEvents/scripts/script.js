c3c7322047f9f2c23fa08f5c0b60a184d26bd58f
Severyn Dorosh Yefimov <severyn00@gmail.com>
2021-10-17 23:24:47 +0100

    Timer works without any unexpected jump, after a username is introduced, its not posible to change it, start button can only me pressed once to play the game, then it's disabled and enabled again after the puzzle is finished\nThe next thing to do is create a matrix and asing the td to it and then start moving pieces to the blank space

--- 2year/DEW/domAndEvents/scripts/script.js
+++ 2year/DEW/domAndEvents/scripts/script.js
@@ -1,3 +1,6 @@
+var timerTime = new Date();
+console.log(timerTime)
+
 var ms = 0;
 var s = 0;
 var m = 0;
@@ -7,7 +10,7 @@ var m = 0;
 function timer(){
     var msAux, sAux, mAux;
     ms += 2;
-    if (ms>100){
+    if (ms>=100){
         s++;
         ms=0;
     }
@@ -20,10 +23,17 @@ function timer(){
     if (m<10){mAux="0"+m;}else{mAux=m;}
     if (ms<10){msAux="0"+ms;}else{msAux=ms;}
 
-    document.getElementById("timer").innerHTML = mAux + ":" + sAux + ":" + msAux; 
+    document.getElementById("timer").innerHTML = mAux + ":" + sAux + ":" + msAux;
+
+
 }
 
 function timerStart(){
+    let userDiv = document.querySelector('#username');
+    let usernameV = document.querySelector('#username input');
+    userDiv.innerHTML = '<input readonly value=' + usernameV.value + '>';
+    let someButton = document.querySelector('#imButton');
+    someButton.innerHTML = '<button>Start</button>';
     timer();
-    setInterval(timer,20);
+    setInterval(timer, 20);
 }
