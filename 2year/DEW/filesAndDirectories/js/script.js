47b2b100a43d8b3bd36040ea769d4204df4224c7
sevensuii <severyn00@gmail.com>
2021-10-29 10:05:37 +0100

    + Some global variables to display icons

--- 2year/DEW/filesAndDirectories/js/script.js
+++ 2year/DEW/filesAndDirectories/js/script.js
@@ -1,3 +1,9 @@
 //Declaration of global variables
-
+var isFile = '<i class="fas fa-file-alt"></i>';
+var closedFolder = '<i class="fas fa-folder"></i>';
+var openedFolder = '<i class="fas fa-folder-open"></i>';
+var addItem = '<i class="fas fa-plus-circle"></i>';
+var removeItem = '<i class="fas fa-minus-circle"></i>';
 //End ---------------------------
+
+//document.querySelector(".icon").innerHTML = openedFolder;
