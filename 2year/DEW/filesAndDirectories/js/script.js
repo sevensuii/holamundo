8e0b2004ddd4c5fc3917d2c568fbf966526e1b60
sevensuii <severyn00@gmail.com>
2021-11-04 13:07:32 +0000

    Finished functions that deletes all type of items and started working on the notification for deleted items.

--- 2year/DEW/filesAndDirectories/js/script.js
+++ 2year/DEW/filesAndDirectories/js/script.js
@@ -16,22 +16,82 @@ function addingFolder() {
     console.log("sad");
 }
 
+//function removeItem(parent1, thisChild1) {
+//    //console.log(e.target.parentElement.parentElement.style.display = "none");
+//    //let parent = e.target.parentElement.parentElement.parentElement;
+//    //let thisChild = e.target.parentElement.parentElement;
+//    //let empty = false;
+//    let parent = parent1;
+//    let thisChild = thisChild1;
+//    console.log(thisChild.childNodes);
+//    if (thisChild.className == "folder") {
+//        for (let h = 0; h < thisChild.childNodes[7].childNodes.length; h++) {
+//            if (thisChild.childNodes[7].childNodes[h].nodeName == "#text") {
+//                empty = true;
+//            }
+//            else {
+//                empty = false;
+//            }
+//        }
+//        if (empty) {
+//            parent.removeChild(thisChild);
+//            
+//        }
+//        console.log("This folder is empty");
+//        
+//    }
+//    else if (thisChild.className == "file" || thisChild.className == "image" || thisChild.className == "pdf"){
+//        console.log("This is a file")
+//        parent.removeChild(thisChild);
+//    }
+//    //console.log(parent);
+//    //console.log(parent.childNodes);
+//}
+
 var lista = document.querySelectorAll(".removeItem");
 
+//for (let i = 0; i < lista.length; i++) {
+//    lista[i].addEventListener("click", function(e) {
+//        let parent = e.target.parentElement.parentElement.parentElement;
+//        let thisChild = e.target.parentElement.parentElement;
+//        removeItem(parent, thisChild);
+//    });
+//}
 for (let i = 0; i < lista.length; i++) {
     lista[i].addEventListener("click", function(e) {
         //console.log(e.target.parentElement.parentElement.style.display = "none");
         let parent = e.target.parentElement.parentElement.parentElement;
         let thisChild = e.target.parentElement.parentElement;
+        let empty = false;
         console.log(thisChild.childNodes);
-        if (thisChild.className == "folder" && thisChild.childNodes[7].length == 0) {
-            console.log("This folder is empty");
-            parent.removeChild(thisChild);
+        if (thisChild.className == "folder") {
+            for (let h = 0; h < thisChild.childNodes[7].childNodes.length - 1; h++) {
+                if (thisChild.childNodes[7].childNodes[h].nodeName == "#text") {
+                    empty = true;
+			let noti = document.querySelector("notification");
+			noti.innerHTML = "The folder has been deleted";
+			noti.style.display = "block";
+			setTimeout(function () {noti.style.display = "none";}, 1500)
+                }
+                else {
+                    empty = false;
+                    break;
+                }
+            }
+            console.log(empty);
+            if (empty) {
+                parent.removeChild(thisChild);
+                
+            }
+            else {
+                console.log("This folder is not empty");
+            }
             
         }
         else if (thisChild.className == "file" || thisChild.className == "image" || thisChild.className == "pdf"){
             console.log("This is a file")
             parent.removeChild(thisChild);
+            empty = false;
         }
         //console.log(parent);
         //console.log(parent.childNodes);
