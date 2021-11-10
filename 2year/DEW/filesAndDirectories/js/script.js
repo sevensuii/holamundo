a0960ed20f764b59fdcf39ecce63bf85cef7b225
sevensuii <severyn00@gmail.com>
2021-11-10 00:57:47 +0000

    Add button now has input field for the new file or folder to be created, needs more work to end.

--- 2year/DEW/filesAndDirectories/js/script.js
+++ 2year/DEW/filesAndDirectories/js/script.js
@@ -2,7 +2,7 @@
 var fileIcon = '<i class="fas fa-file-alt"></i>';
 var closedFolder = '<i class="fas fa-folder"></i>';
 var openedFolder = '<i class="fas fa-folder-open"></i>';
-var addItem = '<i class="fas fa-plus-circle"></i>';
+var addItem = '<i class="fas fa-plus-circle add-btn"></i>';
 var removeItem = '<i class="fas fa-trash"></i>';
 var imageIcon = '<i class="fas fa-image"></i>';
 var pdfIcon = '<i class="fas fa-file-pdf"></i>';
@@ -10,6 +10,9 @@ var pdfIcon = '<i class="fas fa-file-pdf"></i>';
 var folderCheckbox = '<div class="folder"><label class="custom-checkbox"><input type="checkbox" /><i class="fas fa-folder unchecked"></i><i class="fas fa-folder-open checked"></i><span class="text"></span></label><div class="fodler-content"><div class="folder"><label class="custom-checkbox"><input type="checkbox" /><i class="fas fa-folder unchecked"></i><i class="fas fa-folder-open checked"></i><span class="text">Descargas</span></label></div></div>';
 
 var noti = document.querySelector("#notification");
+
+var parent, thisChild, items, myCheckBox;
+var lista = document.querySelectorAll(".removeItem");
 //End ---------------------------
 
 //document.querySelector(".icon").innerHTML = openedFolder;
@@ -18,31 +21,43 @@ function addingFolder() {
     console.log("sad");
 }
 
-var lista = document.querySelectorAll(".removeItem");
-
-for (let i = 0; i < lista.length; i++) {
-    lista[i].addEventListener("click", function(e) {
-        let parent = e.target.parentElement.parentElement.parentElement;
-        let thisChild = e.target.parentElement.parentElement;
-        console.log(thisChild.childNodes);
-        if (thisChild.className == "folder" && thisChild.childNodes[7].childElementCount == 0) {
-            parent.removeChild(thisChild);
-            sendNotification(thisChild.className, 1);
+function addRemoveAction() {
+    for (let i = 0; i < lista.length; i++) {
+        lista[i].addEventListener("click", function(e) {
+            parent = e.target.parentElement.parentElement.parentElement;
+            thisChild = e.target.parentElement.parentElement;
+            console.log(thisChild.children);
+            if (thisChild.className == "folder" && thisChild.childNodes[7].childElementCount == 0) {
+                parent.removeChild(thisChild);
+                sendNotification(thisChild.className, 1);
+            }
+            else if (thisChild.className == "file" || thisChild.className == "image" || thisChild.className == "pdf"){
+                console.log("This file has been removed")
+                parent.removeChild(thisChild);
+                sendNotification(thisChild.className, 1);
         }
-        else if (thisChild.className == "file" || thisChild.className == "image" || thisChild.className == "pdf"){
-            console.log("This file has been removed")
-            parent.removeChild(thisChild);
-            sendNotification(thisChild.className, 1);
+    
+            else {
+                console.log("This folder is not empty");
+                sendNotification(thisChild.className, 0);
+            }
+        })
     }
 
-        else {
-            console.log("This folder is not empty");
-            sendNotification(thisChild.className, 0);
-        }
-    })
 }
 
- function sendNotification(tipe, option) {
+function addAppendAction() {
+    items = document.querySelectorAll(".add-btn");
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', function(e) {
+            console.log(e.target.parentElement.parentElement.children[0]);
+            e.target.parentElement.parentElement.children[0].style.width = "30%"
+            e.target.parentElement.innerHTML = '<input type="text"><input id="add-this-item" type="button" value="Add">';
+        })
+    }
+}
+
+function sendNotification(tipe, option) {
      if (option) {
 	    noti.innerText = "This " + tipe + " has been deleted!";
         noti.style.backgroundColor = "lightgreen";
@@ -57,9 +72,36 @@ for (let i = 0; i < lista.length; i++) {
      }
  }
 
+function addIcons() {
+    items = document.querySelectorAll(".addItem");
+    for (let i = 0; i < items.length; i++) {
+        items[i].innerHTML = addItem;
+    }
+    items = document.querySelectorAll(".removeItem");
+    for (let i = 0; i < items.length; i++) {
+        items[i].innerHTML = removeItem;
+    }
+
+}
+
 function loadEverything() {
 
+    myCheckBox = document.querySelectorAll("div label input");
+    //console.log(myCheckBox);
+    for (let i = 0; i < myCheckBox.length; i++) {
+        myCheckBox[i].addEventListener('change', function(e) {
+            if (myCheckBox[i].checked) {
+                e.target.parentElement.parentElement.childNodes[7].style.display = "block";
+            }
+            else {
+                e.target.parentElement.parentElement.childNodes[7].style.display = "none";
+            }
+        })
+    }
 
+    addIcons();
+    addRemoveAction();
+    addAppendAction();
     
 }
 
@@ -68,24 +110,4 @@ function loadEverything() {
 
 
 
-var items = document.querySelectorAll(".addItem");
-for (let i = 0; i < items.length; i++) {
-    items[i].innerHTML = addItem;
-}
-items = document.querySelectorAll(".removeItem");
-for (let i = 0; i < items.length; i++) {
-    items[i].innerHTML = removeItem;
-}
-var myCheckBox = document.querySelectorAll("div label input");
-console.log(myCheckBox);
-for (let i = 0; i < myCheckBox.length; i++) {
-    myCheckBox[i].addEventListener('change', function(e) {
-        if (myCheckBox[i].checked) {
-            e.target.parentElement.parentElement.childNodes[7].style.display = "block";
-        }
-        else {
-            e.target.parentElement.parentElement.childNodes[7].style.display = "none";
-        }
-    })
-}
 //myCheckBox[i].parentElement.childNodes[7].style.display = 'none';
