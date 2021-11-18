3d997d47d7d3d3ab8518995257f57af2ddd02746
sevensuii <severyn00@gmail.com>
2021-11-18 10:41:54 +0000

    Declaration of a keyboard and load that keyboard into screen keyboard

--- 2year/DEW/virtualKeyboard/javascript/script.js
+++ 2year/DEW/virtualKeyboard/javascript/script.js
@@ -1,5 +1,5 @@
 //Initial commit
-var keyboard = function(){
+var Keyboard = function(){
     this.normalKeys = ['º', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', '¡', 
     'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '`', '+', 
     'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ñ', '´', 'ç',
@@ -7,4 +7,12 @@ var keyboard = function(){
     this.specialKeys = ['esc']
     this.altGrFirstRow = ['\\', '|', '@', '#', '~', '½', '¬', '{', '[', ']', '}', '(', ')'],
     this.shiftFirstRow = ['ª', '"', '·', '$', '%', '&', '/', '(', ')', '=', '?', '¿']
+}
+
+var myWritingZone = document.querySelector('#writing-zone');
+let myKeyboard = new Keyboard();
+let screenKeyboard = document.querySelectorAll('.normal-keys');
+
+for (let i = 0; i < screenKeyboard.length; i++) {
+    screenKeyboard[i].innerText = myKeyboard.normalKeys[i];
 }
