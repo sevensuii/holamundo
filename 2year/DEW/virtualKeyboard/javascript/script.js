5d3699ada5ae874370f1f2dc1e72dad19bfa4036
sevensuii <severyn00@gmail.com>
2021-11-21 15:39:31 +0000

    Keyboard and mouse now should be able to write using both at the same time and only will be written what you see in the virtual keyboard.

--- 2year/DEW/virtualKeyboard/javascript/script.js
+++ 2year/DEW/virtualKeyboard/javascript/script.js
@@ -2,6 +2,7 @@
 //GLOBAL VARIABLES
 var myWritingZone = document.querySelector('#writing-zone');
 var shiftKeyAct = false;
+var physicalShiftKeyAct = false;
 var bloMaKeyAct = false;
 var altGrKeyAct = false;
 
@@ -16,15 +17,15 @@ var Keyboard = function(){
     this.altGrFirstRow = ['\\', '|', '@', '#', '~', '½', '¬', '{', '[', ']', '}', '(', ')'],
     this.shiftFirstRow = ['ª', '!', '"', '·', '$', '%', '&' ,'/', '(', ')', '=', '?', '¿']
     this.screenKeyboard = document.querySelectorAll('.normal-keys');
-    this.tabKey = document.querySelector('#tab');
-    this.spaceKey = document.querySelector('#space');
-    this.backSpaceKey = document.querySelector('#backspace');
-    this.returnKey = document.querySelector('#return');
-    this.escKey = document.querySelector('#esc');
-    this.bloqMayusKey = document.querySelector('#bloq-m');
+    this.tabKey = document.querySelector('#item9');
+    this.spaceKey = document.querySelector('#item32');
+    this.backSpaceKey = document.querySelector('#item8');
+    this.returnKey = document.querySelector('#item8');
+    this.escKey = document.querySelector('#item27');
+    this.bloqMayusKey = document.querySelector('#item20');
     this.shiftKeys = document.querySelectorAll('.shift-keys');
     this.clearKey = document.querySelector('#clear');
-    this.altGrKey = document.querySelector('#r-alt');
+    this.altGrKey = document.querySelector('#item225');
     this.changeKeys = function(val) {  
         switch (val) {
             case 0: //Return to normal
@@ -75,43 +76,61 @@ let myKeyboard = new Keyboard();
 //
 
 //Simple event listeners
+
+//Adds tabulator inside textarea
 myKeyboard.tabKey.addEventListener('click', function() {
     myWritingZone.innerHTML += '\t';
 })
 
+//Adds space inside textarea
 myKeyboard.spaceKey.addEventListener('click', function() {
     myWritingZone.innerHTML += ' ';
 })
 
+//Deletes last character inside textarea
 myKeyboard.backSpaceKey.addEventListener('click', function() {
     myWritingZone.innerHTML = myWritingZone.innerHTML.slice(0, -1);
 })
 
+//Adds new line inside textarea
 myKeyboard.returnKey.addEventListener('click', function() {
     myWritingZone.innerHTML += '\n';
 })
 
+//Returns to normal mode
 myKeyboard.escKey.addEventListener('click', function() {
     myKeyboard.changeKeys(0);
     myKeyboard.bloqMayusKey.style.backgroundColor = '#30aebe';
 })
 
+//Clear everything inside textarea
 myKeyboard.clearKey.addEventListener('click', function() {
     myWritingZone.innerHTML = '';
 })
+
+//When shift is activated and you click outside the keys, shift gets disactivated
+document.addEventListener('click', function() {
+    if (shiftKeyAct || altGrKeyAct) {
+        myKeyboard.changeKeys(0);
+    }
+}, true)
 //End of simple event listeners
 
+//Writes clicked key inside textarea
 for (let i = 0; i < myKeyboard.screenKeyboard.length; i++) {
     myKeyboard.screenKeyboard[i].addEventListener('click', function(e) {
         myWritingZone.innerHTML += e.target.innerHTML;
-        if (bloMaKeyAct) {
-            shiftKeyAct = false;
-            return 0;
-        }
-        if (shiftKeyAct || altGrKeyAct) {
-            shiftKeyAct = false;
-            altGrKeyAct = false;
-            myKeyboard.changeKeys(0);
+        if (physicalShiftKeyAct == false) { //need work
+            if (bloMaKeyAct) {
+                shiftKeyAct = false;
+                return 0;
+            }
+            if (shiftKeyAct || altGrKeyAct) {
+                shiftKeyAct = false;
+                altGrKeyAct = false;
+                myKeyboard.changeKeys(0);
+            }
+            
         }
         /*else if (altGrKeyAct) {
             altGrKeyAct = false;
@@ -125,9 +144,10 @@ for (let i = 0; i < myKeyboard.shiftKeys.length; i++) {
     myKeyboard.shiftKeys[i].addEventListener('click', function() {
         shiftKeyAct = true;
         myKeyboard.changeKeys(1);
-    })
+    }), true
 }
 
+//Blocks everything to upperCase
 myKeyboard.bloqMayusKey.addEventListener('click', function() {
     if (bloMaKeyAct) {
         bloMaKeyAct = false;
@@ -158,10 +178,89 @@ myKeyboard.altGrKey.addEventListener('click', function() {
 //}
 document.addEventListener('keydown', function(e) {
     console.log(e.key);
-    if (myKeyboard.normalKeys.includes(e.key.toLowerCase())) {
-        myWritingZone.innerHTML += e.key;
-    }
-    //console.log(e.keyCode);
+    //if (myKeyboard.normalKeys.includes(e.key.toLowerCase())) {
+    //    myWritingZone.innerHTML += e.key;
+    //}
+    switch (e.keyCode) {
+        case 9: // Tab key
+            e.preventDefault();
+            myWritingZone.innerHTML += '\t';
+            break;
+    
+        case 16:    // Any physical shift key
+            physicalShiftKeyAct = true;
+            myKeyboard.changeKeys(1);
+            myKeyboard.shiftKeys[0].style.backgroundColor = '#16616d';
+            myKeyboard.shiftKeys[1].style.backgroundColor = '#16616d';
+
+            break;        
+
+        case 20:    // Caps Lock key
+            if (bloMaKeyAct) {
+                bloMaKeyAct = false;
+                myKeyboard.bloqMayusKey.style.backgroundColor = '#30aebe';
+                myKeyboard.changeKeys(0);
+            }
+            else {
+                bloMaKeyAct = true;
+                myKeyboard.bloqMayusKey.style.backgroundColor = '#16616d';
+                myKeyboard.changeKeys(1);
+            }
+            break;
+
+        case 8: // Backspace key
+            myWritingZone.innerHTML = myWritingZone.innerHTML.slice(0, -1);
+            break;
+        
+        case 27:    // Esc key
+            myKeyboard.changeKeys(0);
+            myKeyboard.bloqMayusKey.style.backgroundColor = '#30aebe';
+            break;
+            
+        case 32:    // Space key
+            myWritingZone.innerHTML += ' ';
+            break;
+
+        case 13:    // Enter key
+            myWritingZone.innerHTML += '\n';
+            break;
+        
+        case 225:    // Alt gr key
+            myKeyboard.changeKeys(2);
+            break;
+            
+        default:    // Writes inside
+        if (e.key != 'Dead') {
+            if (bloMaKeyAct) {
+                myWritingZone.innerHTML += e.key.toUpperCase();
+            }
+            else {
+                myWritingZone.innerHTML += e.key.toLowerCase();
+            }
+        }
+        break;
+
+        }
+        document.querySelector(`#item${e.keyCode}`).style.backgroundColor = '#16616d';
+    console.log(e.keyCode);
 })
 
+document.addEventListener('keyup', function(e) {
+
+
+    switch (e.keyCode) {
+        case 16:
+            physicalShiftKeyAct = false;
+            myKeyboard.changeKeys(0);
+            myKeyboard.shiftKeys[0].style.backgroundColor = '#30aebe';
+            myKeyboard.shiftKeys[1].style.backgroundColor = '#30aebe';
+            break;
+
+        case 225:    // Alt gr key
+            myKeyboard.changeKeys(0);
+            break;
+    }
+
+    document.querySelector(`#item${e.keyCode}`).style.backgroundColor = '#30aebe';
+})
 //END KEYBOARD EVENT LISTENERS 
