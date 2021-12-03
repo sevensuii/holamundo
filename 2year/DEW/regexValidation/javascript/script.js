860b4c0330a30ac82b7dd7fddea221db329673c4
sevensuii <severyn00@gmail.com>
2021-12-03 09:50:44 +0000

    Finished some validation y added visual effects for iban

--- 2year/DEW/regexValidation/javascript/script.js
+++ 2year/DEW/regexValidation/javascript/script.js
@@ -2,6 +2,9 @@
 const sendButton = document.querySelector('#send');
 const retrieveButton = document.querySelector('#retrieve');
 const inputs =  document.querySelectorAll('input');
+const checkIban = document.querySelector('#iban');
+let itemsValidated = [false, false, false, false, false, false, false, false, false, false, false];
+let contador = 0;
 
 sendButton.addEventListener('click', function(e) {
     e.preventDefault();
@@ -19,17 +22,22 @@ function validate(field, regex){
 
 }
 
-const dni = /^[x]\d{8}[a-z]$/i;
+/* const dni = /^[x]\d{8}[a-z]$/i; */
+/* const dni = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i; */
+const dni = /^[0-9XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
+const nie = /^[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKE]$/i;
 const fName = /^[A-Z]+[a-zA-Z]$/gm;
 const sName = /^(\w+ )+\w+$/gm;
 const valDate = new RegExp("^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$");
-const eMail = /^(?=.{1,81}$)[\w\.-]+@[\w\.-]+\.\w{2,4}$/gm;
+/* const eMail = /^(?=.{1,81}$)[\w\.-]+@[\w\.-]+\.\w{2,4}$/gm; */
+const eMail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 const bDay = /^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$/gm;
 const pCode = /^(?:0[1-9][1-4]\d|5[0-2])\d{2}$/gm;
 const tlfn = /^[679]{1}[0-9]{8}$/gm;
 const iban = /([a-zA-Z]{2})\s\t(\d{2})\s\t(\d{4})\s\t(\d{4})\s\t(\d{2})\s\t(\d{10})/gm;
 const card = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/gm;
-const passwd = /^(?=.[A-Za-z])(?=.\d)(?=.[@$!%#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm;
+/* const passwd = /^(?=.[A-Za-z])(?=.\d)(?=.[@$!%#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm; */
+const passwd = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
 
 inputs.forEach((input) => {
@@ -41,28 +49,34 @@ inputs.forEach((input) => {
         if (e.target.name == 'sname') {
             validate(e.target, sName);
         }
-        if (e.target.name == 'dni') {
-            validate(e.target, dni);
+        if (e.target.name == 'dni') {       /* WORKS */
+                validate(e.target, dni);
         }
-        if (e.target.name == 'bdate') {
+        if (e.target.name == 'bdate') {     /* WORKS */
             validate(e.target, bDay);
         }
         if (e.target.name == 'pcode') {
             validate(e.target, pCode);
         }
-        if (e.target.name == 'email') {
+        if (e.target.name == 'email') {     /* WORKS */
             validate(e.target, eMail);
         }
-        if (e.target.name == 'number') {
+        if (e.target.name == 'number') {    /* WORKS */
             validate(e.target, tlfn);
         }
         if (e.target.name == 'iban') {
             validate(e.target, iban);
+            console.log(e.target.value.length);
+            contador++;
+            if (contador == 4 && e.target.value.length < 19) {
+                e.target.value += '\t';
+                contador = 0;
+            }
         }
-        if (e.target.name == 'passwd') {
+        if (e.target.name == 'passwd') {    /* WORKS */
             validate(e.target, passwd);
         }
-        if (e.target.name == 'passwd1') {
+        if (e.target.name == 'passwd1') {   /* WORKS */
             let pas1 = document.querySelector('#passwd');
             let pas2 = document.querySelector('#passwd1');
 
@@ -78,6 +92,13 @@ inputs.forEach((input) => {
         
     })
 })
+
+/* checkIban.addEventListener('change', function() {
+    console.log(checkIban.value.length);
+    if (checkIban.value.length % 4 == 0) {
+        checkIban.value += ' ';
+    }
+}) */
 
 /* for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('change', function(e) {
