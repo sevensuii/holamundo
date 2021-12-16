001d644102a69c62ab3911cc1c47463a16fe363f
sevensuii <severyn00@gmail.com>
2021-12-16 03:20:37 +0000

    Send, retrieve and fill are working but database not working for now

--- 2year/DEW/ajaxPHP/js/script.js
+++ 2year/DEW/ajaxPHP/js/script.js
@@ -1,80 +1,131 @@
+// /////////////////////////////
+// Declaration of global variables
+////////////////////////////////
+
+var fillForm = document.querySelector('#fill');		// Autofills data to the form
+var inputs = document.querySelectorAll('input');	// Takes all inputs
+
+let sendBut = document.querySelector('#send');
+let retrieveBut = document.querySelector('#retrieve');
+let postDbBut = document.querySelector('#post-db');
+let getDbBut = document.querySelector('#get-db');
+
+let noti = document.querySelector('#noti');
+
+let defaultObj = {
+		name : 'Pepito',
+		surname : 'Lopez Perez',
+		dni : '12345678X',
+		date: '22/09/2000',
+		pcode: '35500',
+		email : 'pepe@gmail.com',
+		hphone : '928666666',
+		number : '666999666',
+		iban : 'ES7921000813610123456789',
+		ccard: '4539955085883327',
+		password: 'Pepe1234567890' 
+	}
+
+// /////////////////////////////
+// End declaration of global variables
+////////////////////////////////
+
+function showData(someObject) {
+	inputs[0].value	= someObject.name; 
+	inputs[1].value	= someObject.surname;
+	inputs[2].value	= someObject.dni;
+	inputs[3].value	= someObject.date;
+	inputs[4].value	= someObject.pcode;
+	inputs[5].value	= someObject.email;
+	inputs[6].value	= someObject.hphone;
+	inputs[7].value	= someObject.number;
+	inputs[8].value	= someObject.iban;
+	inputs[9].value	= someObject.ccard;
+	inputs[10].value = someObject.password;
+}
+
+fillForm.addEventListener('click', function(e) {
+	e.preventDefault();
+    // let fillData = ['Pepito', 'Lopez Perez', '12345678X', '22/09/2000', '35500', 'pepe@gmail.com', '928666666', '666999666', 'ES7921000813610123456789', '4539955085883327', 'Pepe1234567890']
+// 
+    // for (let i = 0; i < inputs.length; i++) {
+	// 	console.log(`${inputs[i].name} |||| ${fillData[i]}`);
+    //     inputs[i].value = fillData[i];
+    // }
+	showData(defaultObj);
+})
+
+sendBut.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	let obj = {
+		name : inputs[0].value,
+		surname : inputs[1].value,
+		dni : inputs[2].value,
+		date: inputs[3].value,
+		pcode: inputs[4].value,
+		email : inputs[5].value,
+		hphone : inputs[6].value,
+		number : inputs[7].value,
+		iban : inputs[8].value,
+		ccard: inputs[9].value,
+		password : inputs[10].value
+	}
+
+	dbParam = JSON.stringify(obj);
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.responseText);
+			myObj = JSON.parse(this.responseText);
+			// document.getElementById("users").innerHTML = myObj.name+ " was stored correctly";
+			noti.innerHTML = myObj.name+ " was stored correctly";
+			noti.style.opacity = 1;
+			setTimeout(function() {noti.style.opacity = 0}, 1500);
+		}
+	};
+	xmlhttp.open("POST", "http://localhost/normal.php", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send("x=" + dbParam);
+})
 
 
-/*
-document.write("one ");
-document.write("PAUSE ");
-document.write("two ");
-document.write("three ");
-document.write("four ");
-document.write("five ");
-document.write("one ");
-setTimeout(function (){document.write("PAUSE ");}, 6000);
-document.write("two ");
-document.write("three ");
-document.write("four ");
-document.write("five ");
- */
-//http://www.filltext.com/
-//https://www.w3schools.com/js/js_ajax_intro.asp
-var animalContainer = document.getElementById("animal-info");
-var btn = document.getElementById("btn");
+retrieveBut.addEventListener("click", function(e) {
+	e.preventDefault();
 
-btn.addEventListener("click", function() {
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://raw.githubusercontent.com/etielprofe/dew/main/animals.json');
-  ourRequest.onload = function() {
-    if (ourRequest.status >= 200 && ourRequest.status < 400) {//https://www.w3schools.com/tags/ref_httpmessages.asp
-      var ourData = JSON.parse(ourRequest.responseText);
-      console.log(ourRequest.responseText);
-      animalContainer.innerText+=ourData[0].species;
-      animalContainer.innerText+=ourData[1].species;
-      animalContainer.innerText+=ourData[2].species;
-    } else {
-      console.log("We connected to the server, but it returned an error.");
-    }
-    
-  };
+	var xmlhttp = new XMLHttpRequest();
 
-  ourRequest.onerror = function() {
-    console.log("Connection error");
-  };
+	xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		console.log(this.responseText);
+		myObj = JSON.parse(this.responseText);
+		
+		// document.getElementById("demo").innerHTML = myObj.name + " was received correctly";
+		noti.innerHTML = myObj.name+ " was received correctly";
+		noti.style.opacity = 1;
+		setTimeout(function() {noti.style.opacity = 0}, 1500);
+		showData(myObj);
+	}
+	};
+	xmlhttp.open("GET", "http://localhost/normal.php", true);
+	xmlhttp.send();
 
-  ourRequest.send();
-});
-/*DATA AT SERVER
-[
-  {
-    "name": "Meowsy",
-    "species" : "cat",
-    "foods": {
-      "likes": ["tuna", "catnip"],
-      "dislikes": ["ham", "zucchini"]
-    }
-  },
-  {
-    "name": "Barky",
-    "species" : "dog",
-    "foods": {
-      "likes": ["bones", "carrots"],
-      "dislikes": ["tuna"]
-    }
-  },
-  {
-    "name": "Purrpaws",
-    "species" : "cat",
-    "foods": {
-      "likes": ["mice"],
-      "dislikes": ["cookies"]
-    }
-  }
-]
-*/
+})
 
-let fillForm = document.querySelector('#fill');
-fillForm.addEventListener('click', function() {
-    let fillData = ['Pepe', 'Lopez Perez', '12345678X', '22/09/2000', '35500', 'pepe@gmail.com', '928666666', '666999666', '4539955085883327', 'ES7921000813610123456789', 'Pepe1234567890']
-    let inputs = document.querySelectorAll('input');
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i] = fillData[i];
-    }
+postDbBut.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	console.log ('Asking server with POST method...');
+	// var params = "user="+u+"&message="+m;
+	let params = "name"+inputs[0].value+"&surname"+inputs[1].value+"&dni"+inputs[2].value+"&date"+inputs[3].value+"&pcode"+inputs[4].value+"&email"+inputs[5].value+"&hphone"+inputs[6].value+"&number"+inputs[7].value+"&iban"+inputs[8].value+"&ccard"+inputs[9].value+"&password"+inputs[10].value;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST','http://localhost/workdb.php', true);
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');//add an HTTP header with setRequestHeader(). Specify the data you want to send in the send() method:
+
+	xhr.onload = function(){
+	console.log(this.responseText);
+	}
+
+	xhr.send(params);
 })
