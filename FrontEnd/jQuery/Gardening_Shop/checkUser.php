753a7c014d6b9c3c3b5191f19a1189485f77388f
sevensuii <severyn00@gmail.com>
2022-02-21 01:19:54 +0000

    Checks if the user exists

---
+++ FrontEnd/jQuery/Gardening_Shop/checkUser.php
@@ -0,0 +1,43 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "tienda";
+    
+    $usuario = $_REQUEST['usuario'] ?? '';
+
+    try {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    //   $conn = new mysqli("mysql:host=$servername;dbname=tienda", $username, $password);
+      // set the PDO error mode to exception
+    //   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //   echo "Connected successfully";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+
+    $sql = "SELECT usuario FROM clientes WHERE usuario LIKE $usuario;";
+    $resultado = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    
+    
+    // $return_arr = array();
+    
+    while ($file = mysqli_fetch_assoc($resultado)) {
+        $usu1 = $file['usuario'] ?? false;
+        // echo $usu1;
+        if ($usuario <=> $usu1) {
+            echo 'true';
+        }
+    }
+    
+    // // var_dump($return_arr);
+
+
+    // $isJSON = json_encode($return_arr);
+    // echo $isJSON;
+
+
+
+
+
+?>
